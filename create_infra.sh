c524231239fed23011adbb683757929d35ae409e
Ubuntu <ubuntu@ip-172-31-0-69.us-west-2.compute.internal>
2021-06-19 15:29:52 +0000

    Adding create_infra.sh

--- create_infra.sh
+++ create_infra.sh
@@ -2,4 +2,4 @@
 
 echo "Creating Infrastructure"
 
-aws cloudformation create-stack --stack-name P5-Stack --template-body file://create_stack.yml --region us-west-2 --on-failure DO_NOTHING
+aws cloudformation create-stack --stack-name P5Stack --template-body file://create_stack.yml --region us-west-2 --on-failure DO_NOTHING
